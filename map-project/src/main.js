e145fd5b207502b162b5ed33cc130ee76f4235e8
Eregion <wuxize1995@gmail.com>
2023-10-31 19:55:22 -0400

    add google map api

--- map-project/src/main.js
+++ map-project/src/main.js
@@ -1,21 +1,4 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import Vue from 'vue'
-import * as VueGoogleMaps from 'vue2-google-maps'
-
-Vue.config.productionTip = false
-dotenv.config();
-const googleMapsApiKey = process.env.GOOGLE_MAPS_API_KEY;
-
-
-const app = createApp(App);
-app.use(VueGoogleMaps, {
-  load: {
-    key: googleMapsApiKey,
-    libraries: 'places',
-  }
-});
-app.mount('#app');
+createApp(App).mount('#app')
