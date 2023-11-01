cb3611879bb9fd4d8bc9f6f89285d0d3b552caed
Eregion <wuxize1995@gmail.com>
2023-11-01 14:18:10 -0400

    re-write with Google Place Searches Sample as template

--- map-project/src/App.vue
+++ map-project/src/App.vue
@@ -1,91 +1,69 @@
 <template>
-  <h1>Google Map App</h1>
   <div>
-    <div id="map" style="width: 75%; height: 500px"></div>
-    <input id="pac-input" ref="searchInput" type="text" placeholder="Search for a location" />
+    <h1>Google Map App</h1>
+    <div>
+      <div id="map" style="width: 100%; height: 400px;"></div>
+    </div>
   </div>
-  <GoogleMap api-key="AIzaSyBT-zccmJ_6zmyO5utOPC5b_eUBrTQL3AA" :center="center" :zoom="15">
-    <Marker :options="{ position: center }" />
-  </GoogleMap>
 </template>
 
-
 <script>
-import { defineComponent, ref, onMounted } from "vue";
-import { GoogleMap, Marker } from "vue3-google-map";
-
-export default defineComponent({
-  components: { GoogleMap, Marker },
-  setup() {
-    const center = ref({ lat: -33.8688, lng: 151.2195 });
-
-    onMounted(() => {
-      const mapElement = document.querySelector("#map");
-      const searchInputElement = document.getElementById("pac-input");
-      const searchBox = new google.maps.places.SearchBox(searchInputElement);
-      const markers = [];
-
-      const map = new google.maps.Map(mapElement, {
-        center: center.value,
-        zoom: 13,
-        mapTypeId: "roadmap",
-      });
-
-      map.controls[google.maps.ControlPosition.TOP_LEFT].push(searchInputElement);
-
-      map.addListener("bounds_changed", () => {
-        searchBox.setBounds(map.getBounds());
-      });
-
-      searchBox.addListener("places_changed", () => {
-        const places = searchBox.getPlaces();
-
-        if (places.length === 0) {
-          return;
-        }
-
-        markers.forEach((marker) => {
-          marker.setMap(null);
-        });
-
-        markers.length = 0;
-        const bounds = new google.maps.LatLngBounds();
-
-        places.forEach((place) => {
-          if (!place.geometry || !place.geometry.location) {
-            console.log("Returned place contains no geometry");
-            return;
-          }
-
-          const icon = {
-            url: place.icon,
-            size: new google.maps.Size(71, 71),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(17, 34),
-            scaledSize: new google.maps.Size(25, 25),
-          };
-
-          markers.push(
-            new google.maps.Marker({
-              map,
-              icon,
-              title: place.name,
-              position: place.geometry.location,
-            })
-          );
-
-          if (place.geometry.viewport) {
-            bounds.union(place.geometry.viewport);
-          } else {
-            bounds.extend(place.geometry.location);
-          }
-        });
-
-        map.fitBounds(bounds);
-      });
-    });
-
-    return { center };
+export default {
+  data() {
+    return {
+      map: null,
+      service: null,
+      infowindow: null,
+      markers: [],
+      request: {
+        query: 'Arsenal FC London',
+        fields: ['name', 'geometry'],
+      },
+    };
   },
-});
+  mounted() {
+    // Define the global initMap function
+    window.initMap = this.initMap;
+
+    // Check if the Google Maps API has already been loaded. Re-write with error handler later
+    if (typeof google !== 'undefined') {
+      this.initMap();
+      console.log("Fuiyoh! You have a functional Google Map API!");
+    } else {
+      console.log("Haiyaa... Google Map API has not been loaded. Is your API key valid?");
+    }
+  },
+  methods: {
+    initMap() {
+      const sydney = new google.maps.LatLng(-33.867, 151.195);
+
+      this.infowindow = new google.maps.InfoWindow();
+      this.map = new google.maps.Map(this.$el.querySelector('#map'), {
+        center: sydney,
+        zoom: 15,
+      });
+
+      this.service = new google.maps.places.PlacesService(this.map);
+      this.service.findPlaceFromQuery(this.request, (results, status) => {
+        if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+          for (let i = 0; i < results.length; i++) {
+            this.createMarker(results[i]);
+          }
+
+          this.map.setCenter(results[0].geometry.location);
+        }
+      });
+    },
+    createMarker(place) {
+      if (!place.geometry || !place.geometry.location) return;
+
+      const marker = new google.maps.Marker({
+        map: this.map,
+        position: place.geometry.location,
+      });
+
+      marker.place = place;
+    },
+  },
+};
 </script>
